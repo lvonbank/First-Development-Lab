b1d92b0d3138ecbc5eb673d786259a10852dac99
lvonbank <31550897+lvonbank@users.noreply.github.com>
2019-01-20 12:46:47 -0600

    Add Code

---
+++ GFUDemo_CS/GFUDemo_CS/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GFUDemo_CS
+{
+    public class Class1
+    {
+    }
+
+    public class Automobile
+    {
+        public Automobile()
+        {
+            this.Model = "Not specified";
+            this.TopSpeed = -1;
+            this.IsRunning = true;
+        }
+
+        public Automobile(string model, int topSpeed)
+        {
+            Model = model;
+            TopSpeed = topSpeed;
+        }
+
+        public bool IsRunning { get; set; }
+        public string Model { get; set; }
+        public int TopSpeed { get; set; }
+
+        public void Start()
+        {
+            if (this.Model != "Not specified" || this.TopSpeed != -1)
+                this.IsRunning = true;
+            else
+                this.IsRunning = false;
+        }
+    }
+}
---
+++ GFUDemo_CS/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GFUDemo_CS;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AutomobileTest
+    {
+        [TestMethod]
+        public void DefaultAutomobileIsInitializedCorrectly()
+        {
+            Automobile myAuto = new Automobile();
+            Assert.IsTrue((myAuto.Model == "Not specified") && (myAuto.TopSpeed == -1));
+        }
+        [TestMethod]
+        public void AutomobileWithModelNameCanStart()
+        {
+            string model = "Contoso";
+            int topSpeed = 199;
+            Automobile myAuto = new Automobile(model, topSpeed);
+
+            myAuto.Start();
+            Assert.IsTrue(myAuto.IsRunning == true);
+        }
+    }
+}
